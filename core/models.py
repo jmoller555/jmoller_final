15ee74d723d3599b191b4c90dc8fbed34f7fd9bd
jmoller <jacquelyn.moller@gmail.com>
2015-11-17 17:20:08 +0000

    Add request create view

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Request(models.Model):
+  course_code = models.CharField(max_length=300)
+  topic_description = models.TextField(null=True, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  user = models.ForeignKey(User)
+
+  def __unicode__(self):
+    return self.course_code
--- core/views.py
+++ core/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.core.urlresolvers import reverse_lazy
+from .models import *
 
 # Create your views here.
 class Home(TemplateView):
   template_name = "home.html"
+
+class RequestCreateView(CreateView):
+  model = Request
+  template_name = "request/request_form.html"
+  fields = ['course_code', 'topic_description']
+  success_url = reverse_lazy('home')
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super(RequestCreateView, self).form_valid(form)
