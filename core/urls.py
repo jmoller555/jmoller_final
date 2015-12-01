b6c74c05ec6207658477da13e8880699d9f3a7e9
jmoller <jacquelyn.moller@gmail.com>
2015-12-01 19:22:21 +0000

    Add request list view

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
   url(r'^user/', include('registration.backends.simple.urls')),
   url(r'^user/', include('django.contrib.auth.urls')),
   url(r'^request/create/$', RequestCreateView.as_view(), name='request_create'),
+  url(r'^request/$', RequestListView.as_view(), name='request_list'),
 )
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, ListView
 from django.core.urlresolvers import reverse_lazy
 from .models import *
 
@@ -11,8 +11,12 @@ class RequestCreateView(CreateView):
   model = Request
   template_name = "request/request_form.html"
   fields = ['course_code', 'topic_description']
-  success_url = reverse_lazy('home')
+  success_url = reverse_lazy('request_list')
 
   def form_valid(self, form):
     form.instance.user = self.request.user
     return super(RequestCreateView, self).form_valid(form)
+
+class RequestListView(ListView):
+  model = Request
+  template_name = "request/request_list.html"
